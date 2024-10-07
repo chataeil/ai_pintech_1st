d60cada119971e6e43ea5564c29f492fab87479b
yonggyo1125 <ckxodlf12@naver.com>
2024-10-07 16:53:14 +0900

    day01 수업 소스

---
+++ 4.자바 기초/day01/src/exam01/Ex02.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int numberOfStudent;
+        int num2 = 20; // 선언과 동시에 초기화
+
+
+
+    }
+}
---
+++ 4.자바 기초/day01/src/exam01/Ex07.java
@@ -0,0 +1,9 @@
+package exam01;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        long num1 = 1000000000000L; // 처음부터 long으로 인식
+        long num3 = 10_000_000_000L;
+        System.out.println(num3);
+    }
+}
